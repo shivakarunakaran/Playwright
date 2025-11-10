c9553317123932ef0dac51d1b8c9a187813fee7e
Shiva Karunakaran <letter2karan@gmail.com>
2025-11-10 16:42:51 +0530

    Initial commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,4 +3,8 @@ import { test, expect } from '@playwright/test';
 test('example test', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
+
+
+
+  
 });
